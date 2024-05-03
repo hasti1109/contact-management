e2618ca8aca2053197907418a741e4973603a7e0
Hasti Gabani <hastigabani1109@gmail.com>
2024-05-03 13:52:36 +0530

    minor changes

--- server.js
+++ server.js
@@ -3,12 +3,11 @@ const errorHandler = require("./middleware/errorHandler")
 const dotenv = require("dotenv").config()
 const app = express()
 const connectDb = require('./config/dbConnection')
-
 const port = process.env.PORT || 5000
 
 connectDb();
 
-app.use(express.json()) //middleware that provides a parser which parses data received from client
+app.use(express.json())
 
 app.use("/api/contacts", require("./routes/contactRoutes"));
 
